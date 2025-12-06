103f592c8c9fdf182668acd1de9d6a5afc96ee20
SPACEGG <spacegg@github.com>
2025-12-06 06:49:03 +0000

    fix: remove cookiegen

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ import { Collection } from 'discord.js';
 import db from '#src/database.js';
 import client from '#src/client.js';
 import logger from '#src/logger.js';
-import config from '#src/config.js';
-import getCookies from '#src/cookies.js';
-import { getPOT } from '#src/utils.js';
+// import config from '#src/config.js';
+// import getCookies from '#src/cookies.js';
+// import { getPOT } from '#src/utils.js';
 
 import { commandCollections } from '#src/deployments/commands.js';
 // import { devCommandCollections } from '#src/deployments/dev-commands.js';
 import '#src/deployments/events.js';
 
 // cookies
-try {
-    await getCookies();
-    logger.info('Successfully generated cookies.');
-} catch (e) {
-    logger.error(`CookiesError: ${e}`);
-}
+// try {
+//     await getCookies();
+//     logger.info('Successfully generated cookies.');
+// } catch (e) {
+//     logger.error(`CookiesError: ${e}`);
+// }
 
 // PO Token
 // try {
