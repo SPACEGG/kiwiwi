ba606125c4fdb666f2e864a86e257b663778fd30
SPACEGG <spacegg@github.com>
2026-03-06 01:30:18 +0900

    feat: Update docker-entrypoint to check latest yt-dlp release an update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# Update yt-dlp to the latest version on startup
+echo "Updating yt-dlp..."
+yt-dlp -U || echo "yt-dlp update failed, continuing with current version."
+
 echo "wait db server"
 wait-for-it db:3306 -t 20 -- echo "db is up"
 
